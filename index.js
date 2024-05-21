471eaebc424458a27914bb1a49a533e7fe4bd192
Takezo123 <daniilbulatkin@gmail.com>
2024-05-21 23:56:11 +0300

    начало бд

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const express = require('express')
+import express from 'express';
+import jwt from 'jsonwebtoken';
 const app = express()
 
-app.set('view engine', 'ejs')
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.render('index')
 })
-app.get('/about', (req, res) => {
-    res.render('about')
+
+app.post('/user/login', (req, res) => {
+    console.log(req.body);
+    const token = jwt.sign({}
+    res.json(
+        { "status": "success",
+            "message": "User logged in successfully"}
+    )
 })
-app.get('/user/:name', (req, res) => {
-    let data ={username: req.params.name, hobbies: ['football', 'basketball']}
-    res.render('user',data)
-})
+
+// app.get('/about', (req, res) => {
+//     res.render('about')
+// })
+
+// app.get('/user/:name', (req, res) => {
+//     let data ={username: req.params.name, hobbies: ['football', 'basketball']}
+//     res.render('user',data)
+// })
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
