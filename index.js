13aee5cf3bc5e7073ed36cb4fd4bdeca01e5ab76
Takezo123 <daniilbulatkin@gmail.com>
2024-05-21 23:06:12 +0300

    учусь

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const http = require('http')
-const fs = require('fs')
+const express = require('express')
+const app = express()
 
+app.set('view engine', 'ejs')
 
-let server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-    if (req.url === '/')
-        fs.createReadStream('./templates/index.html').pipe(res)
-    else if (req.url === '/about')
-        fs.createReadStream('./templates/about.html').pipe(res)
-    else
-        fs.createReadStream('./templates/404.html').pipe(res)
+app.get('/', (req, res) => {
+    res.render('index')
 })
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/user/:name', (req, res) => {
+    let data ={username: req.params.name, hobbies: ['football', 'basketball']}
+    res.render('user',data)
+})
+const PORT = process.env.PORT || 3000
 
-
-const PORT = 3000
-const HOST = '127.0.0.1'
-server.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`)
+app.listen(PORT, () => {
+    console.log('Server is running on port:'+ PORT)
 })
