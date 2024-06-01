a7fbaadbd807e9641ba773d1a4c6683d09e2159c
Takezo123 <daniilbulatkin@gmail.com>
2024-06-01 19:56:31 +0300

    Revert "shit_show"
    This reverts commit 4c06032f03a92b08d311373186bcbd5b33cb206f.

--- routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createProduct, getProducts } from '../controllers/PostController.js';
-
-const router = express.Router();
-
-router.post('/products', createProduct);
-router.get('/products', getProducts);
-
-export default router;
