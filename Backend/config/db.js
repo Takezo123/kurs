635ef763e0d171c66ab71bb3a360f082db16c8e5
Takezo123 <daniilbulatkin@gmail.com>
2024-06-14 18:23:42 +0300

    a

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://admin:admin@cluster0.9ww5wjm.mongodb.net/retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://admin:admin@cluster0.9ww5wjm.mongodb.net/dodoptica').then(()=>console.log("DB Connected"));
 }
