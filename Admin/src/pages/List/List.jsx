de876d07819e14425df1b40eef15533eebb44e61
Takezo123 <daniilbulatkin@gmail.com>
2024-06-20 11:17:32 +0300

    1

--- Admin/src/pages/List/List.jsx
+++ Admin/src/pages/List/List.jsx
@@ -1,23 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import './List.css'
-import axios from "axios"
-import {toast} from "react-toastify"
-import SaleForm from '../Sale/Sale.jsx'
-const List = ({url}) => {
+import React, { useEffect, useState } from 'react';
+import './List.css';
+import axios from "axios";
+import { toast } from "react-toastify";
+import SaleForm from '../Sale/Sale.jsx';
+import foodModel from '../../../../Backend/models/foodModel.js';
 
+
+
+const List = ({ url }) => {
   const [selectedItems, setSelectedItems] = useState([]);
-  const [list,setList]=useState([]);
+  const [list, setList] = useState([]);
 
-  const fetchList=async ()=>{
-    const response=await axios.get(`${url}/api/food/list`);
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error("Error");
+      }
+    } catch (error) {
+      toast.error("Error");
+    }
+  };
 
-    if(response.data.success){
-      setList(response.data.data)
-    }
-    else{
-      toast.error("Error")
-    }
-  }
   const handleItemSelect = (itemId) => {
     setSelectedItems((prevSelectedItems) => {
       if (prevSelectedItems.includes(itemId)) {
@@ -27,27 +33,28 @@ const List = ({url}) => {
       }
     });
   };
-  const removeFood= async (foodId)=>{
-    const response = await axios.post(`${url}/api/food/delete`,{id:foodId});
-    await fetchList();
-    if(response.data.success){
-      toast.success(response.data.message);
-    }
-    else{
-      toast.error("Error");
-    }
-  }
 
-  useEffect(()=>{
+  const removeFood=async(req,res)=>{
+    try {
+        const food=await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success:true,message:"Food Removed"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})        
+    }
+}
+
+  useEffect(() => {
     fetchList();
-  },[])
-
+  }, []);
 
   return (
-
-<div className='list add flex-col'>
-    <p>Список всех продуктов</p>
-    <div className="list-table">
+    <div className='list add flex-col'>
+      <p>Список всех продуктов</p>
+      <div className="list-table">
         <div className="list-table-format title">
           <b>Image</b>
           <b>Name</b>
@@ -56,26 +63,26 @@ const List = ({url}) => {
           <b>Action</b>
         </div>
         {list.map((item, index) => {
-  const isSelected = selectedItems.includes(item._id);
-  return (
-    <div key={index} className="list-table-format">
-      <input
-        type="checkbox"
-        checked={isSelected}
-        onChange={() => handleItemSelect(item._id)}
-      />
-      <img src={`${url}/images/`+item.image} alt=''/>
-      <p>{item.name}</p>
-      <p>{item.category}</p>
-      <p>${item.price}</p>
-      <p onClick={() => removeFood(item._id)} className='cursor'>X</p>
-    </div>
-  )
-})}
+          const isSelected = selectedItems.includes(item._id);
+          return (
+            <div key={index} className="list-table-format">
+              <input
+                type="checkbox"
+                checked={isSelected}
+                onChange={() => handleItemSelect(item._id)}
+              />
+              <img src={`${url}/images/` + item.image} alt='' />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={() => removeFood(item._id)} className='cursor'>X</p>
+            </div>
+          )
+        })}
       </div>
       <SaleForm url={url} selectedItems={selectedItems} />
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
